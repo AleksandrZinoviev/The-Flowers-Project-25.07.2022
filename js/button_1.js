294255f01b4724707fd3197d5427d2b51a2a8e71
TasmanianCat <serbulatov@bk.ru>
2022-07-25 17:14:18 +0400

    25.07.2022

---
+++ js/button_1.js
@@ -0,0 +1,7 @@
+function clickedButton_1() {
+    console.log('The Button was clicked!')
+
+    document.getElementById('button-1').style.backgroundColor = 'red';
+    document.getElementById('button-1').style.color = 'white';
+    document.getElementById('button-1').style.borderRadius = '1rem';
+}
